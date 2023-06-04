f171f4857e7f8e5f010d47299bbbe46761bea5d2
hannes <hannesdelbeke@gmail.com>
2023-06-04 14:30:40 +0100

    cleanup, fix, tested

--- plugget_qt_addon/__init__.py
+++ plugget_qt_addon/__init__.py
@@ -12,47 +12,27 @@ bl_info = {
 import bpy
 
 
-# Define a new menu class
-# class MyMenu(bpy.types.Menu):
-#     bl_idname = "OBJECT_MT_my_menu"
-#     bl_label = "My Menu"
-
-#     def draw(self, context):
-#         layout = self.layout
-#         layout.operator("object.my_operator")
-
-
-# Define a new operator class
-class MyOperator(bpy.types.Operator):
-    bl_idname = "object.my_operator"
-    bl_label = "My Operator"
+class OpenPluggetQt(bpy.types.Operator):
+    """Open the Plugget Qt Manager window"""
+    bl_idname = "plugget.show_qt_manager"
+    bl_label = "Open Plugget Qt Manager"
 
     def execute(self, context):
-        # Your code here
         import plugget_search_widget as psw
         psw.show()
         return {'FINISHED'}
 
 
-# Add the new menu item to the "Window" menu
 def menu_func(self, context):
-#     self.layout.menu(MyMenu.bl_idname)
-    self.layout.operator(MyOperator.bl_idname, icon="MESH_CUBE")
+    # Add the new menu item to the "Window" menu
+    self.layout.operator(OpenPluggetQt.bl_idname, icon="MESH_CUBE")
 
 
 def register():
-    # bpy.utils.register_class(MyMenu)
-    bpy.utils.register_class(MyOperator)
-#    bpy.types.VIEW3D_MT_object.append(menu_func)
+    bpy.utils.register_class(OpenPluggetQt)
     bpy.types.TOPBAR_MT_window.append(menu_func)
 
 
 def unregister():
-    # bpy.utils.unregister_class(MyMenu)
-    bpy.utils.unregister_class(MyOperator)
-#    bpy.types.VIEW3D_MT_object.remove(menu_func)
+    bpy.utils.unregister_class(OpenPluggetQt)
     bpy.types.TOPBAR_MT_window.remove(menu_func)
-
-
-# if __name__ == "__main__":
-#     register()
